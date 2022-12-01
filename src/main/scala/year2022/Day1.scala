9c0ff3c87891f09db8a4de9ec8c7bb6f393249aa
tothpeti <toth.peter96@digikabel.hu>
2022-12-01 10:00:54 +0100

    Add Day1 solution

--- src/main/scala/year2022/Day1.scala
+++ src/main/scala/year2022/Day1.scala
@@ -1,7 +1,51 @@
 package year2022
 
 import cats.effect.{IO, IOApp}
+import fs2.Chunk
+import fs2.io.file.{Files, Path}
 
 object Day1 extends IOApp.Simple {
-  override def run: IO[Unit] = ???
+
+  // private val testPath = "src/main/resources/year2022/day1/test.txt"
+  // private val part1Path = "src/main/resources/year2022/day1/part1.txt"
+  private val part2Path = "src/main/resources/year2022/day1/part2.txt"
+
+  private def calculateSumOfCaloriesByChunks(calories: Chunk[String]): IO[Int] =
+    IO {
+      calories
+        .map(_.toInt)
+        .foldLeft(0)(_ + _)
+    }
+
+  private def getTopKhighestCalories(calories: List[Int], k: Int): IO[Int] =
+    IO {
+      calories
+        .sortWith(_ > _)
+        .take(k)
+        .sum
+    }
+
+  private def processedInput(path: String): IO[List[Int]] =
+    Files[IO]
+      .readUtf8Lines(Path(path))
+      .split(_ == "")
+      .evalMap(calculateSumOfCaloriesByChunks)
+      .compile
+      .toList
+
+  override def run: IO[Unit] =
+    // Part 1 solution
+    /*
+    for {
+      list <- processedInput(part1Path)
+      _    <- IO(println(list.max))
+    } yield ()
+     */
+
+    // Part 2 solution
+    for {
+      list <- processedInput(part2Path)
+      res  <- getTopKhighestCalories(list, 3)
+      _    <- IO(println(res))
+    } yield ()
 }
